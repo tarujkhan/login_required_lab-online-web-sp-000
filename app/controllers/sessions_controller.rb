bb20a6924db345c2c10db94c7ad5d4f921c8ddd5
taroj khan <tarujkhan@yahoo.com>
2021-03-10 17:01:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     return require_login if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    #redirect_to controller: 'application', action: 'hello'
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
