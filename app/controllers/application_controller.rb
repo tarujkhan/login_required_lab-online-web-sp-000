838f57e55b1c0edae0a7bae28efa4cc1c1b9564a
taroj khan <tarujkhan@yahoo.com>
2021-03-10 15:56:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= Session.find(session[:name]) if session[:name]
+    @current_user = session[:name]
   end
 
   def hello
